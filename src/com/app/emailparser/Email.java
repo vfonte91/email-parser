7d83a6425de126b8b33e185e23bd7f05cd69fb6a
Vince Fonte <v.fonte91@gmail.com>
2018-05-08 21:42:49 -0600

    Updated Email.java

--- src/com/app/emailparser/Email.java
+++ src/com/app/emailparser/Email.java
@@ -1,8 +1,25 @@
 package com.app.emailparser;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Email {
-	public Email(File input_File) {
+	private List<String> text = new ArrayList<String>();
+
+	public Email(File input_File) throws FileNotFoundException {
+		Scanner s = new Scanner(input_File);
+		ArrayList<String> list = new ArrayList<String>();
+		while (s.hasNextLine()){
+		    list.add(s.nextLine());
+		}
+		s.close();
+		text = list;
+	}
+
+	public String printOutput(String output) {
+		return "Foo!";
 	}
 }
