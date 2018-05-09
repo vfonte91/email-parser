a0f4ec9d7197835e618c30760da8bcbb90948298
Vince Fonte <vfonte@cas.org>
2018-05-09 13:01:59 -0600

    Added function for getting from class

--- src/com/app/emailparser/Email.java
+++ src/com/app/emailparser/Email.java
@@ -1,7 +1,10 @@
 package com.app.emailparser;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -9,20 +12,40 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Email {
-	private File email_file = new File();
+	private List<String> content;
+	private String file_name;
 
-	public Email(File input_File) throws FileNotFoundException {
-		Scanner s = new Scanner(input_File);
-		ArrayList<String> list = new ArrayList<String>();
-		while (s.hasNextLine()){
-		    list.add(s.nextLine());
-		}
-		s.close();
-		byte[] encoded = Files.readAllBytes(Paths.get(input_File));
-		return new String(encoded, encoding);
+	public Email(File input_file) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(input_file));
+		content = new ArrayList<String>();
+		String line;
+		while ((line = br.readLine()) != null)
+			content.add(line);
+	}
+
+	public Email(List<String> content, String file_name) {
+		this.content = content;
+		this.file_name = file_name;
 	}
 	
 	public String printOutput(String output) {
 		return "Foo!";
+	}
+
+	public void printContents() {
+		for (String line : content)
+			System.out.println(line);
+	}
+
+	public String getFromAddress() {
+		String results = "";
+		for (String line  : content) {
+			if (line.contains("From: ")) {
+				line = line.replace("From: ", "").replaceAll(".*<", "").replaceAll(">", "");
+				results = line;
+				break;
+			}
+		}
+		return results;
 	}
 }
