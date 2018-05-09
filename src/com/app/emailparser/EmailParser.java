adb2a968d3603cc634cd7b6868e888fd69afa0c8
Vince Fonte <v.fonte91@gmail.com>
2018-05-08 18:21:03 -0600

    Created src/com/app/emailparser/EmailParser.java

---
+++ src/com/app/emailparser/EmailParser.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.app.emailparser;
+
+/**
+ * @author Vince Fonte
+ *
+ */
+public class EmailParser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
